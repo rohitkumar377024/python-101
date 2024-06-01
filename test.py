a2888586b8b6a6fd0ae2469c3615bef2ce27be76
Rohit Kumar <ruhi@Rohits-MacBook-Air.local>
2024-06-02 00:48:08 +0530

    Initial Commit - Python 101

---
+++ test.py
@@ -0,0 +1,11 @@
+# Fizz Buzz
+
+# Take input from user for a number
+print('Enter a number:')
+num = input()
+
+# If divisible by 3 and 5, print FizzBuzz
+
+# If divisible by 3, print Fizz
+
+# If divisible by 5, print Buzz
